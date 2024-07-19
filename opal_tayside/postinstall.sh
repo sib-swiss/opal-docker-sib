fdcfce281f966fcf67ed1cf783a1136b46932c50
Iulian Dragan <iulian_dragan@yahoo.com>
2024-07-19 09:21:51 +0200

    fix db name

--- opal_tayside/postinstall.sh
+++ opal_tayside/postinstall.sh
@@ -6,4 +6,4 @@ opal rest -o http://localhost:8080 --user administrator --password  $OPAL_ADMINI
 opal rest /datashield/package/dsSwissKnife/_publish?profile=default --opal http://localhost:8080 --user administrator --password  $OPAL_ADMINISTRATOR_PASSWORD --method PUT
 opal rest /datashield/package/resourcex/_publish?profile=default --opal http://localhost:8080 --user administrator --password  $OPAL_ADMINISTRATOR_PASSWORD --method PUT
 opal rest /datashield/package/dsQueryLibraryServer/_publish?profile=default --opal http://localhost:8080 --user administrator --password  $OPAL_ADMINISTRATOR_PASSWORD --method PUT
-opal rest -o http://localhost:8080 --user administrator --password  $OPAL_ADMINISTRATOR_PASSWORD  --method POST "/permissions/datashield?type=GROUP&permission=DATASHIELD_USEW&principal=$OPAL_PRIVILEGED_GROUP"
+opal rest -o http://localhost:8080 --user administrator --password  $OPAL_ADMINISTRATOR_PASSWORD  --method POST "/system/permissions/datashield?type=GROUP&permission=DATASHIELD_USE&principal=$OPAL_PRIVILEGED_GROUP"
