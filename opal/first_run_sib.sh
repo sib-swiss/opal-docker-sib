7f1f70d4af6fd4b321db37e57eded60cbb0fd4a4
Iulian Dragan <iulian_dragan@yahoo.com>
2025-11-11 11:08:01 +0100

    generic container with data first attempt

---
+++ opal/first_run_sib.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set_property () {
+  field=$1
+  value=$2
+  file=$3
+  tmpfile=/tmp/`basename ${file}`
+  if grep -q ${field} ${file}
+  then
+    sed -r "s#${field}\s*=.*#${field}=${value}#g" ${file} | \
+      sed -r "s/^#${field}=/${field}=/g" > ${tmpfile}
+  else
+    cat ${file} > ${tmpfile}
+    echo "${field} = ${value}" >> ${tmpfile}
+  fi
+  mv ${tmpfile} ${file}
+}
+# start with a pause to allow rock to finish
+sleep 30
+echo '{"name": "Opal","languages": ["en"],"defaultCharSet": "ISO-8859-1","enforced2FA": false,"allowRPackageManagement": true}' | opal rest -o http://localhost:8080 --user administrator --password  $OPAL_ADMINISTRATOR_PASSWORD --method PUT "/system/conf/general" --content-type="application/json"
+opal rest -o http://localhost:8080 --user administrator --password  $OPAL_ADMINISTRATOR_PASSWORD --method PUT "/datashield/packages/_publish?profile=default&name=dsQueryLibraryServer&name=dsSwissKnife&name=resourcex&name=dsMOdual&name=dsMOprimal&name=dsBase&name=dsLCMM&name=dsImputation"
+opal project -o http://localhost:8080 -u administrator -p $OPAL_ADMINISTRATOR_PASSWORD --add --name $PROJECT_NAME 
+sed s/@host@/$POSTGRESDATA_HOST/g /opt/opal/data/resource.json | \
+      sed s/@user@/$POSTGRESDATA_USER/g | \	
+      sed s/@password@/$POSTGRESDATA_PASSWORD/g | \
+      sed s/@port@/$POSTGRESDATA_PORT/g | \
+      sed s/@dbname@/$PROJECT_NAME/g | \
+opal rest -o http://localhost:8080 -u administrator -p $OPAL_ADMINISTRATOR_PASSWORD -m POST /project/$PROJECT_NAME/resources --content-type "application/json"
+opal rest -o http://localhost:8080 --user administrator --password  $OPAL_ADMINISTRATOR_PASSWORD --method POST "/project/$PROJECT_NAME/permissions/resources?type=GROUP&permission=RESOURCES_VIEW&principal=$OPAL_PRIVILEGED_GROUP"
+opal perm-datashield --opal http://localhost:8080 --user administrator --password $OPAL_ADMINISTRATOR_PASSWORD --type GROUP --subject $OPAL_PRIVILEGED_GROUP --permission use --add
--- postgres/2_populate_test_db.sh
+++ postgres/2_populate_test_db.sh
@@ -3,14 +3,18 @@ set -e
 export PGPASSWORD=$POSTGRES_PASSWORD
 
 # load the database
-psql -v ON_ERROR_STOP=1 --username postgres --dbname obelisk -f /scripts/OMOPCDM_postgresql_5.4_ddl.sql
-psql -v ON_ERROR_STOP=1 --username postgres --dbname obelisk -f /scripts/OMOPCDM_postgresql_5.4_indices.sql
-psql -v ON_ERROR_STOP=1 --username postgres --dbname obelisk -f /scripts/OMOPCDM_postgresql_5.4_primary_keys.sql
-psql -v ON_ERROR_STOP=1 -U postgres -d obelisk -c "GRANT SELECT ON ALL TABLES IN SCHEMA public TO opal; ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO opal;"
-psql -v ON_ERROR_STOP=1 -U postgres -d obelisk -c "\copy concept from '/scripts/CONCEPT.csv' with(FORMAT CSV,HEADER,  DELIMITER E'\t', QUOTE '\')"
-psql -v ON_ERROR_STOP=1 -U postgres -d obelisk -c "\copy domain from '/scripts/DOMAIN.csv' with(FORMAT CSV,HEADER,  DELIMITER E'\t', QUOTE '\')"
-psql -v ON_ERROR_STOP=1 -U postgres -d obelisk -c "\copy domain from '/scripts/VOCABULARY.csv' with(FORMAT CSV,HEADER,  DELIMITER E'\t', QUOTE '\')"
+psql -v ON_ERROR_STOP=1 --username postgres --dbname seeddb -f /scripts/OMOPCDM_postgresql_5.4_ddl.sql
+psql -v ON_ERROR_STOP=1 --username postgres --dbname seeddb -f /scripts/OMOPCDM_postgresql_5.4_indices.sql
+psql -v ON_ERROR_STOP=1 --username postgres --dbname seeddb -f /scripts/OMOPCDM_postgresql_5.4_primary_keys.sql
+psql -v ON_ERROR_STOP=1 -U postgres -d seeddb -c "GRANT SELECT ON ALL TABLES IN SCHEMA public TO opal; ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO opal;"
+psql -v ON_ERROR_STOP=1 -U postgres -d seeddb -c "\copy concept from '/scripts/CONCEPT.csv' with(FORMAT CSV,HEADER,  DELIMITER E'\t', QUOTE '\')"
+psql -v ON_ERROR_STOP=1 -U postgres -d seeddb -c "\copy domain from '/scripts/DOMAIN.csv' with(FORMAT CSV,HEADER,  DELIMITER E'\t', QUOTE '\')"
+psql -v ON_ERROR_STOP=1 -U postgres -d seeddb -c "\copy domain from '/scripts/VOCABULARY.csv' with(FORMAT CSV,HEADER,  DELIMITER E'\t', QUOTE '\')"
 rm /scripts/CONCEPT.csv
 rm /scripts/DOMAIN.csv
 rm /scripts/VOCABULARY.csv
-#psql -v ON_ERROR_STOP=1 --username postgres --dbname obelisk -f /scripts/OMOPCDM_postgresql_5.4_constraints.sql
+for dir in /path/to/parent/*/; do
+	cd $dir
+	/bin/bash load_$dir.sh
+done
+#psql -v ON_ERROR_STOP=1 --username postgres --dbname seeddb -f /scripts/OMOPCDM_postgresql_5.4_constraints.sql
